772993b8cc8d916fc4caa4d5d10ea49b10684842
3vi1john <jbabio@me.com>
2014-04-03 17:01:55 -0400

    Update start-snorby.sh

--- start-snorby.sh
+++ start-snorby.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -r /var/www/snorby/tmp/pids/server.pid
+killall -9 ruby1.9.1
 cd /var/www/snorby
 bundle exec rails server -e production &
 ruby script/delayed_job start RAILS_ENV=production &
